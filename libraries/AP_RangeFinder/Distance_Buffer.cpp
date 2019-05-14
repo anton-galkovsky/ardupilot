6bfea3459e128950e5b56ed445d0eac465b9a765
anton-galkovsky <galkovsky_anton@mail.ru>
2019-05-14 16:23:26 +0300

    Added buffered distance manager

---
+++ libraries/AP_RangeFinder/Distance_Buffer.cpp
@@ -0,0 +1,70 @@
+/*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_Common/AP_Common.h>
+#include <AP_HAL/AP_HAL.h>
+#include "RangeFinder_Backend.h"
+#include "Distance_Buffer.h"
+
+#include <GCS_MAVLink/GCS.h>
+#include <stdio.h>
+
+Distance_Buffer::Distance_Buffer(AP_RangeFinder_Backend *sens) {
+	next_ind = 0;
+	last_measure_time_ms = 1;
+	penult_measure_time_ms = 0;
+	last_value_cm = 0;
+	penult_value_cm = 0;
+	value_derivative_cmms = 0.0f;
+	sensor = sens;
+}
+
+void Distance_Buffer::add_value(uint16_t val, uint32_t sensor_last_reading_ms) {
+	penult_measure_time_ms = last_measure_time_ms;
+	last_measure_time_ms = sensor_last_reading_ms;
+
+	buf[next_ind] = val;
+	next_ind = (next_ind + 1) % DISTANCE_BUFFER_LENGTH;
+
+	memcpy(sorted_buf, buf, DISTANCE_BUFFER_LENGTH * sizeof(uint16_t));
+	qsort(sorted_buf, DISTANCE_BUFFER_LENGTH, sizeof(uint16_t), compare);
+
+	penult_value_cm = last_value_cm;
+	last_value_cm = sorted_buf[DISTANCE_BUFFER_LENGTH / 2];
+//	gcs().send_text(MAV_SEVERITY_CRITICAL, "%d ", last_value_cm);
+
+	value_derivative_cmms = (last_value_cm - penult_value_cm)
+			/ (last_measure_time_ms - penult_measure_time_ms);
+}
+
+void Distance_Buffer::add_sensor_data(uint32_t time) {
+	uint32_t sensor_last_reading_ms = sensor->last_reading_ms();
+	if (time - sensor_last_reading_ms > 30) {
+	    sensor_last_reading_ms = time;
+	}
+	if (last_measure_time_ms == sensor_last_reading_ms) {
+		return;
+	}
+
+	uint16_t sensor_data;
+	if (sensor->has_data()) {
+		sensor_data = sensor->distance_cm();
+	} else {
+		sensor_data = sensor->max_distance_cm() - 1;
+	}
+	gcs().send_text(MAV_SEVERITY_CRITICAL, "%d ", sensor_data);
+	add_value(sensor_data, sensor_last_reading_ms);
+}
+
---
+++ libraries/AP_RangeFinder/Distance_Buffer.h
@@ -0,0 +1,52 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "RangeFinder.h"
+
+#define DISTANCE_BUFFER_LENGTH 4
+
+class Distance_Buffer {
+
+    static int compare(const void * a, const void * b) {
+        return *((float*)a) - *((float*)b);
+    }
+
+	const AP_RangeFinder_Backend *sensor;
+
+	uint16_t buf[DISTANCE_BUFFER_LENGTH] {};
+	uint16_t sorted_buf[DISTANCE_BUFFER_LENGTH] {};
+	uint8_t next_ind;
+	uint32_t last_measure_time_ms;
+	uint32_t penult_measure_time_ms;
+	uint16_t last_value_cm;
+	uint16_t penult_value_cm;
+	float value_derivative_cmms;
+
+	void add_value(uint16_t val, uint32_t sensor_last_reading_ms);
+
+public:
+
+	Distance_Buffer(AP_RangeFinder_Backend *sensor);
+
+	void add_sensor_data(uint32_t time);
+
+	//return median
+	uint16_t get_dist_cm() const { return last_value_cm; }
+
+	uint16_t get_derivative_cmms() const { return value_derivative_cmms;	}
+
+	uint32_t get_last_measure_time_ms() const { return last_measure_time_ms; }
+};
